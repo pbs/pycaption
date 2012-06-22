853c4ee5b73a6b0178c47250f971d4923524a32e
Joe Norton <joey@nortoncrew.com>
2012-06-22 09:12:19 -0400

    Added proper python setup structure

---
+++ pycaption/__init__.py
@@ -0,0 +1,22 @@
+
+"""
+pycaption
+~~~~~~~~
+
+:copyright: (c) 2012 by PBS
+:license: Apache 2.0
+
+"""
+
+__title__ = 'pycaption'
+__version__ = '0.10'
+__author__ = 'Joe Norton'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2012 PBS'
+
+from .pycaption import CaptionConverter
+from .dfxp import DFXPWriter, DFXPReader
+from .sami import SAMIReader, SAMIWriter
+from .srt import SRTReader, SRTWriter
+from .scc import SCCReader
+from .transcript import TranscriptWriter
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'README.md')
+
+dependencies = [
+    'nltk',
+    'beautifulsoup',
+    'cssutils'
+]
+
+setup(
+    name='pycaption',
+    version='0.1',
+    description='Multi-language caption reader/writer',
+    author='Joe Norton',
+    author_email='joey@nortoncrew.com',
+    url='https://github.com/pbs/pycaption',
+    install_requires = dependencies,
+    packages = find_packages(),
+    include_package_data=True,
+)
