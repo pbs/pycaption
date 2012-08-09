32b7a06b87d3e50e0a6d3fb6e8dbfdac555b3eb1
Joe Norton <joey@nortoncrew.com>
2012-08-09 11:23:39 -0400

    Updated version to 0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='0.1',
+    version='0.2',
     description='Closed caption converter',
     author='Joe Norton',
     author_email='joey@nortoncrew.com',
