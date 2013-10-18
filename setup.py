c6711403b311051bc4424ee107ce0f3257cb785c
Ion Scerbatiuc <ion.scerbatiuc@3pillarglobal.com>
2013-10-18 10:03:24 +0300

    Bump version to 0.2.13

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='0.2.12',
+    version='0.2.13',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
