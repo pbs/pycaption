9b01072eb1dd7af2ebe0e37690e08cfe1e8c7474
vladiibine <vlad.ardelean@3pillarglobal.com>
2014-11-12 11:47:57 +0200

    Ignoring timing errors for the generated WebVTT filees, with the explicit option of not ignoring them

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='0.4.4',
+    version='0.4.5',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
