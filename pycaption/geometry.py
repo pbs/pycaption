358e02a369c52a487ab9ad2e64e50acc589731e6
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-02-06 15:04:17 +0200

    Prevented attribute errors

--- pycaption/geometry.py
+++ pycaption/geometry.py
@@ -69,7 +69,10 @@ class Stretch(TwoDimensionalObject):
 
     def serialized(self):
         """Returns a tuple of the useful attributes of this object"""
-        return self.horizontal.serialized(), self.vertical.serialized()
+        return (
+            None if not self.horizontal else self.horizontal.serialized(),
+            None if not self.vertical else self.vertical.serialized()
+        )
 
 
 class Region(object):
@@ -181,7 +184,10 @@ class Point(TwoDimensionalObject):
     def serialized(self):
         """Returns the "useful" values of this object.
         """
-        return self.x.serialized(), self.y.serialized()
+        return (
+            None if not self.x else self.x.serialized(),
+            None if not self.y else self.y.serialized()
+        )
 
 
 class Size(object):
@@ -328,10 +334,10 @@ class Padding(object):
         """Returns a tuple containing the useful values of this object
         """
         return (
-            self.before.serialized(),
-            self.after.serialized(),
-            self.start.serialized(),
-            self.end.serialized()
+            None if not self.before else self.before.serialized(),
+            None if not self.after else self.after.serialized(),
+            None if not self.start else self.start.serialized(),
+            None if not self.end else self.end.serialized()
         )
 
 
@@ -364,7 +370,7 @@ class Layout(object):
         """Returns nested tuple containing the "useful" values of this object
         """
         return (
-            self.origin.serialized(),
-            self.extent.serialized(),
-            self.padding.serialized()
+            None if not self.origin else self.origin.serialized(),
+            None if not self.extent else self.extent.serialized(),
+            None if not self.padding else self.padding.serialized()
         )
