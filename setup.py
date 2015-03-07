17bb144fcd60c98769bd972f271116666202e535
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2015-03-07 15:35:49 +0100

    Add a simple command line interface
    Can be run as 'pycaption' or by calling 'python -m pycaption'

--- setup.py
+++ setup.py
@@ -23,6 +23,11 @@ setup(
     install_requires=dependencies,
     packages=find_packages(),
     include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pycaption = pycaption.cli:main',
+        ],
+    },
     classifiers=[
         'Topic :: Multimedia :: Video',
     ],
