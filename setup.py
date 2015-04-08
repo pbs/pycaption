cd0c80ff09c5439a48d1ddfec9cbc7a1275cb9fe
Ariel Pontes <ariel.pontes@3pillarglobal.com>
2015-04-08 17:30:42 +0300

    Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='0.4.6',
+    version='0.5.0',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
