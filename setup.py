d2c4d2c0e6d46ad5e050ebf3e02f314926852fd7
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-05-14 10:55:46 +0300

    Version Bump to 0.5.1c1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='0.5.1',
+    version='0.5.1c1',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
