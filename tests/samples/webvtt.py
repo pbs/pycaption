674602cb0f6a9465b0bb2477ae0b912382f092d5
Ariel Pontes <ariel.pontes@3pillarglobal.com>
2015-05-26 14:08:33 +0300

    Moved the last four SAMItoWebVTT tests to SAMIReaderTestCase (since they weren't related to WebVTT output specifically).

--- tests/samples/webvtt.py
+++ tests/samples/webvtt.py
@@ -196,19 +196,3 @@ WEBVTT
 aa
 bb
 """
-
-SAMPLE_WEBVTT_FROM_SAMI_WITH_BAD_SPAN_ALIGN = u"""WEBVTT
-
-00:00.133 --> 00:04.133 align:right
-Some say we have this vision of Einstein as an old, wrinkly man
-"""
-
-SAMPLE_WEBVTT_FROM_SAMI_WITH_BAD_DIV_ALIGN = SAMPLE_WEBVTT_FROM_SAMI_WITH_BAD_SPAN_ALIGN
-
-SAMPLE_WEBVTT_FROM_SAMI_WITH_P_ALIGN = u"""WEBVTT
-
-00:00.133 --> 00:04.133 align:right
-Some say we have this vision of Einstein as an old, wrinkly man
-"""
-
-SAMPLE_WEBVTT_FROM_SAMI_WITH_P_AND_SPAN_ALIGN = SAMPLE_WEBVTT_FROM_SAMI_WITH_P_ALIGN
