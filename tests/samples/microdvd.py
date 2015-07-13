3ac87ffe136e8a7c4ab20ca3afe0a7eb96e01c27
Samuli Tuomola <samuli.tuomola@gmail.com>
2015-07-13 14:48:00 +0300

    Support for MicroDVD format

---
+++ tests/samples/microdvd.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+SAMPLE_MICRODVD = u"""{230}{307}( clock ticking )
+{371}{433}MAN:|When we think|of E equals m c-squared,
+{433}{468}we have this vision of Einstein
+{468}{522}as an old, wrinkly man|with white hair.
+{522}{669}MAN 2:|E equals m c-squared is|not about an old Einstein.
+{669}{754}It's actually about|a young, energetic, dynamic,
+{754}{805}even a sexy Einstein.
+{841}{953}ACTOR AS EINSTEIN:|What would I see if I rode|on a beam of light?
+{1087}{1137}MAN:|Perhaps some sort
+{1137}{1197}of electrical force is emanating
+{1197}{1224}outwards from|the wire.
+{1224}{1255}What?
+"""
+
+SAMPLE_MICRODVD_EMPTY = u"""
+"""
