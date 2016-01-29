116a1ff5b912bff9db7017023d41c88eb0ea7058
Sebastian Annies <sebastian.annies@gmail.com>
2016-01-29 08:21:34 +0100

    repaired frame acurate timing
    (cherry picked from commit 0b5b615f60dfab5cea187c1f63aff1f69432e08f)

--- pycaption/dfxp/base.py
+++ pycaption/dfxp/base.py
@@ -142,13 +142,16 @@ class DFXPReader(BaseReader):
 
             secsplit = timesplit[2].split('.')
             if len(timesplit) > 3:
-                secsplit.append((int(timesplit[3]) / 30) * 100)
+                secsplit.append((float(timesplit[3]) / 30 * 1000000))
             while len(secsplit[1]) < 3:
                 secsplit[1] += '0'
             microseconds = (int(timesplit[0]) * 3600000000 +
                             int(timesplit[1]) * 60000000 +
                             int(secsplit[0]) * 1000000 +
                             int(secsplit[1]) * 1000)
+            if (len(secsplit) > 2):
+                microseconds += int(secsplit[2])
+
             return microseconds
         else:
             # Must be offset-time
