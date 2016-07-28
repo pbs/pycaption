69209540848c485fcdff727caf3557077f891a87
James Turk <james.p.turk@gmail.com>
2016-07-28 11:18:18 -0400

    bump to 0.6.0 - python2 version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='0.5.6',
+    version='0.6.0',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
