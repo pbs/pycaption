0da8a005749e4365adda5e3f4165690a43edcc0a
Blaine Jester <blainesworld@gmail.com>
2019-04-12 17:09:42 -0700

    Ensure style blocks are ordered as read, update sample for sorted declarations

--- pycaption/base.py
+++ pycaption/base.py
@@ -330,7 +330,7 @@ class CaptionSet(object):
         return self._styles.get(selector, {})
 
     def get_styles(self):
-        return sorted(self._styles.items())
+        return self._styles.items()
 
     def set_styles(self, styles):
         self._styles = styles
