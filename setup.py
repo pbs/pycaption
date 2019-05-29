a1c3b2cedc62b0d4762293ca9d0f4f24a07f35bd
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2019-05-29 10:23:13 +0300

    Bumpversion 0.7.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='0.7.6',
+    version='0.7.7',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
