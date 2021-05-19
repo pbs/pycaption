eab473a1e97e7b58e52fa18ce8cbe7ddf799b36c
Ana Nichifor <ana.nichifor@3pillarglobal.com>
2021-05-19 11:45:47 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ dependencies = [
 
 setup(
     name='pycaption',
-    version='1.0.3',
+    version='1.0.4',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
