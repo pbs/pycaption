d85afac0fd9de7be72c7d3180b4bc4e24f1246ad
Paul Volosin <paul.volosin@3pillarglobal.com>
2021-08-17 13:35:45 +0300

    OCTO-10161. Fix missing comma in project's list of classifiers.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Topic :: Software Development :: Libraries'
+        'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Multimedia :: Video',
     ],
