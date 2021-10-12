d5484d33672336a334dcf0827e7c5e8e5c3d49dc
Ana Nichifor <ana.nichifor@3pillarglobal.com>
2021-10-12 13:02:30 +0300

    OCTO-10201 Restored tab offsets in fixtures, replaced them with 8080 no-op chars where they affected expected timing and posiitioning, small refactoring in checking the type of command

--- pycaption/scc/__init__.py
+++ pycaption/scc/__init__.py
@@ -321,8 +321,8 @@ class SCCReader(BaseReader):
         # first check if word is a command
         # TODO - check that all the positioning commands are here, or use
         # some other strategy to determine if the word is a command.
-        if word in COMMANDS or _is_pac_command(word) \
-                or word in PAC_TAB_OFFSET_COMMANDS:
+        if word in (COMMANDS or PAC_TAB_OFFSET_COMMANDS) \
+                or _is_pac_command(word):
             self._translate_command(word)
 
         # second, check if word is a special character
