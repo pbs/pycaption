ef1fe4554fbc40d3553a6454af2ad85b1b7160ca
calin.marchis <calin.marchis@3pillarglobal.com>
2022-12-21 16:56:58 +0200

    update pycaption version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ transcript_dependencies = [
 
 setup(
     name='pycaption',
-    version='2.1.0',
+    version='2.1.1',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
