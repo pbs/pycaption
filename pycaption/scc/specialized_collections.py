31e1105189aad6655ea3378a853b242952ba7566
Andrej Peterka <andrej.peterka@castlabs.com>
2023-06-29 16:44:20 +0200

    (FIX) Fix running under Python 3.10

--- pycaption/scc/specialized_collections.py
+++ pycaption/scc/specialized_collections.py
@@ -149,7 +149,12 @@ class NotifyingDict(dict):
         :param observer: any callable that can be called with 2 positional
             arguments
         """
-        if not isinstance(observer, collections.Callable):
+        try:
+            check_type = collections.Callable
+        except AttributeError:
+            # python 3.10+
+            check_type = collections.abc.Callable
+        if not isinstance(observer, check_type):
             raise TypeError('The observer should be callable')
 
         self.observers.append(observer)
