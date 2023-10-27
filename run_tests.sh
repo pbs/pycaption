d43da1f2cc3ccc56edfeffc57787ec252804e027
OlteanuRares <rares.olteanu@3pillarglobal.com>
2023-10-27 12:43:55 +0300

    remove useless django config

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
+#!/bin/bash -eux
 
-echo "$1"
+DOCKER_CMD="docker-compose -p pycaption"
 
-if [ $1 == "test_py37" -o $1 == "test_py38"  -o $1 == "test_py39"  ]; then
-    docker compose up $1
-else
-  docker compose up test_py39
+SERVICE="test_py39"
+
+if [ "$@" ]; then
+  if [ "$1" == "test_py37" ] || [ "$1" == "test_py38" ]  || [ "$1" == "test_py39"  ]; then
+    SERVICE="$1"
+  fi
 fi
+
+$DOCKER_CMD build "$SERVICE"
+
+function cleanup {
+    echo "Cleaning up ..."
+    $DOCKER_CMD down && $DOCKER_CMD rm -fv
+}
+
+$DOCKER_CMD run --rm "$SERVICE"
+
+cleanup
