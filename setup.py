ec23873cd12e6dbcd5d88eaed2cd702dfa5f715f
OlteanuRares <rares.olteanu@3pillarglobal.com>
2023-11-01 11:01:31 +0200

    change project version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ transcript_dependencies = [
 
 setup(
     name='pycaption',
-    version='2.2.0dev1',
+    version='2.2.0',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
