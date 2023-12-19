0e22f96c411bcc2af1149181f9c4da2162840990
OlteanuRares <rares.olteanu@3pillarglobal.com>
2023-12-19 16:50:06 +0200

    bump version for publishing into testpypi

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ transcript_dependencies = [
 
 setup(
     name='pycaption',
-    version='2.2.0',
+    version='2.2.0.dev',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
