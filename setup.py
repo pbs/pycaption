ffccb0def0ca2202ccf6500ca7bc4830dc85f7b9
OlteanuRares <rares.olteanu@3pillarglobal.com>
2024-01-09 17:39:15 +0200

    bump version to 2.2.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ transcript_dependencies = [
 
 setup(
     name='pycaption',
-    version='2.2.1.dev',
+    version='2.2.1',
     description='Closed caption converter',
     long_description=open(README_PATH).read(),
     author='Joe Norton',
