4b2d07224a070db4b20080e756ad12bd6a01e091
OlteanuRares <rares.olteanu@3pillarglobal.com>
2024-02-01 14:23:44 +0200

    OCTO-10927-skip-duplicated-extended-characters

--- tests/test_scc.py
+++ tests/test_scc.py
@@ -279,7 +279,8 @@ class TestCoverageOnly:
             'WE SERVE.',
             '®°½',
             'ABû',
-            'ÁÁÉÓ¡',
+            # 'ÁÁÉÓ¡',
+            'ÁÉÓ¡',
             "WHERE YOU'RE STANDING NOW,",
             "LOOKING OUT THERE, THAT'S AL",
             'THE CROWD.',
@@ -288,6 +289,9 @@ class TestCoverageOnly:
             'And wildlife.',
             '>> Bike Iowa, your source for',
         ]
+        print("==========")
+        from pprint import pprint
+        pprint(actual_texts)
         assert expected_texts == actual_texts
 
     def test_multiple_formats(self, sample_scc_multiple_formats):
