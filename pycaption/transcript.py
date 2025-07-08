f24e02fa43f636f76fee4c3e8f6dd5fbff1cfde6
Jay Thompson <eyemyth@users.noreply.github.com>
2025-07-08 12:58:32 -0500

    update nltk; patch CVE-2024-39705

--- pycaption/transcript.py
+++ pycaption/transcript.py
@@ -1,35 +1,33 @@
-import os
-
-try:
-    import nltk.data
-except ModuleNotFoundError:
-    nltk = None
-
 from pycaption.base import BaseWriter, CaptionNode
 
 
 class TranscriptWriter(BaseWriter):
     def __init__(self, *args, **kw):
-        if not nltk:
-            raise ModuleNotFoundError('Missing Dependency You must install nltk ')
-        self.nltk = nltk.data.load(f'file:{os.path.dirname(__file__)}'
-                                   '/english.pickle')
+        try:
+            from nltk import PunktSentenceTokenizer
+
+            self.tokenizer = PunktSentenceTokenizer()
+        except ModuleNotFoundError as exc:
+            raise ModuleNotFoundError(
+                "Missing Dependency: You must install nltk"
+            ) from exc
 
     def write(self, captions):
         transcripts = []
 
         for lang in captions.get_languages():
-            lang_transcript = ''
+            lang_transcript = ""
 
             for caption in captions.get_captions(lang):
-                lang_transcript = self._strip_text(caption.nodes,
-                                                   lang_transcript)
+                lang_transcript = self._strip_text(caption.nodes, lang_transcript)
 
-            lang_transcript = '\n'.join(self.nltk.tokenize(lang_transcript))
+            lang_transcript = "\n".join(self.tokenizer.tokenize(lang_transcript))
             transcripts.append(lang_transcript)
 
-        return '\n'.join(transcripts)
+        return "\n".join(transcripts)
 
     def _strip_text(self, elements, lang_transcript):
-        return ' '.join([lang_transcript] + [el.content for el in elements if
-                                             el.type_ == CaptionNode.TEXT])
+        return " ".join(
+            [lang_transcript]
+            + [el.content for el in elements if el.type_ == CaptionNode.TEXT]
+        )
