e93635712dcd63024a039bc9a23d3bdc2a46946b
OlteanuRares <rares.olteanu@3pillarglobal.com>
2026-04-23 13:50:38 +0300

    bump version, update changelog and Readme.rst

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ transcript_dependencies = ["nltk==3.9.1"]
 
 setup(
     name="pycaption",
-    version="2.2.20",
+    version="2.2.21.dev1",
     description="Closed caption converter",
     long_description=open(README_PATH).read(),
     author="Joe Norton",
