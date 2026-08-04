6c367bcce082006955ab1ae7c8100c035e53f3f3
OlteanuRares <rares.olteanu@3pillarglobal.com>
2026-08-04 16:39:25 +0300

    bumb version for testpypi

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ transcript_dependencies = ["nltk==3.10.0"]
 
 setup(
     name="pycaption",
-    version="2.3.2",
+    version="2.3.3.dev1",
     description="Closed caption converter",
     long_description=open(README_PATH).read(),
     author="Joe Norton",
