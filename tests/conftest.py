cde0f36e67b705245b5f6b4fb5db36231b5887ea
eduard.panasicov <eduard.panasicov@3pillarglobal.com>
2026-09-14 15:05:01 +0300

    OCTO-11566 Add noqa unused imports in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,7 +51,7 @@ from tests.fixtures.dfxp import (  # noqa: F401
     sample_dfxp_without_region_and_style,
 )
 from tests.fixtures.microdvd import missing_fps_sample_microdvd  # noqa: F401
-from tests.fixtures.microdvd import (
+from tests.fixtures.microdvd import (  # noqa: F401
     sample_microdvd,
     sample_microdvd_2,
     sample_microdvd_empty,
@@ -59,7 +59,7 @@ from tests.fixtures.microdvd import (
     sample_microdvd_invalid_format,
 )
 from tests.fixtures.sami import sample_sami  # noqa: F401
-from tests.fixtures.sami import (
+from tests.fixtures.sami import (  # noqa: F401
     sample_sami_double_br,
     sample_sami_empty,
     sample_sami_empty_cue_output,
@@ -142,7 +142,7 @@ from tests.fixtures.scc import (  # noqa: F401
     scc_that_generates_webvtt_with_proper_newlines,
 )
 from tests.fixtures.srt import sample_srt_ascii  # noqa: F401
-from tests.fixtures.srt import (
+from tests.fixtures.srt import (  # noqa: F401
     sample_srt,
     sample_srt_blank_lines,
     sample_srt_empty,
